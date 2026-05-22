fd48eec2c9b141af40fbdbbfe1c8f5290f001afb
MorriganDM <pedrovicthorcordeiro@gmail.com>
2026-05-22 16:22:44 -0300

    Extract header, hero, about into components.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './components/header/header';
+import { Hero } from './components/hero/hero';
+import { About } from './components/about/about';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Hero],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
