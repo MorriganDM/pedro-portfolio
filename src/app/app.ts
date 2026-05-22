eb482d90e0cacf4d787be420153172c64230b41d
MorriganDM <pedrovicthorcordeiro@gmail.com>
2026-05-22 16:28:25 -0300

    Extract projects section into component

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { Header } from './components/header/header';
 import { Hero } from './components/hero/hero';
 import { About } from './components/about/about';
+import { Projects } from './components/projects/projects';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Hero],
+  imports: [RouterOutlet, Header, Hero, About, Projects],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
